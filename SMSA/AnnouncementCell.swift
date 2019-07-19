5463d16dd998f0dfb2113c452772475184bb3252
Pavly Habashy <phabashy@usc.edu>
2019-07-19 15:47:15 -0700

    idk just messin around

---
+++ SMSA/AnnouncementCell.swift
@@ -0,0 +1,14 @@
+//
+//  AnnouncementCell.swift
+//  SMSA
+//
+//  Created by Pavly Habashy on 7/19/19.
+//  Copyright © 2019 SMSA Devs. All rights reserved.
+//
+
+import UIKit
+
+class AnnouncementCell: UITableViewCell {
+    @IBOutlet weak var announcImage: UIImageView!
+    @IBOutlet weak var announcTitle: UILabel?
+}
