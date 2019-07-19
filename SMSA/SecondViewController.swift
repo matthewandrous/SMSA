10b1c32e3910fe02a3c32256fcda917581ec91c9
matthewandrous <androus@usc.edu>
2019-07-19 14:15:04 -0700

    Initial Commit

---
+++ SMSA/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SMSA
+//
+//  Created by Matthew Androus on 7/19/19.
+//  Copyright © 2019 SMSA Devs. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
