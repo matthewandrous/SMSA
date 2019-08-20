abb1c91aa18e552e12fc5e997a74eeb5913416bf
Pavly Habashy <phabashy@usc.edu>
2019-08-20 13:37:23 -0700

    Added comments for LiveStream file

--- SMSA/Controller/LiveStreamViewController.swift
+++ SMSA/Controller/LiveStreamViewController.swift
@@ -11,43 +11,52 @@ import WebKit
 
 class LiveStreamViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
     
-//    var video: Video = Video()
-    
     @IBOutlet weak var videoWebView: WKWebView!
     @IBOutlet weak var Activity: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Live stream URL
         let url = URL(string: "https://www.youtube.com/embed/live_stream?channel=UC_eGnmI2MqiwOLncMVLKbOg")
+        
+        // Used to check if the live stream is loading
         videoWebView.navigationDelegate = self
+        
+        // Start loading URL
         videoWebView.load(URLRequest(url: url!))
+        
+        // Start the activity indicator
         self.Activity.startAnimating()
-        self.videoWebView.navigationDelegate = self
+        
+        // Will hide the element when the loading is done
         self.Activity.hidesWhenStopped = true
 
     }
     
+    // Delegate method for the loading animation
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         Activity.stopAnimating()
     }
     
+    // Delegate method for the loading animation
     func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
         Activity.stopAnimating()
     }
     
     
-    // Not working - Don't know what should be pass in as the string
+    // 'Open in YouTube' button
     @IBAction func onOpenYouTube(_ sender: UIButton) {
-        playInYoutube(youtubeId: "UC_eGnmI2MqiwOLncMVLKbOg")
-    }
-    
-    func playInYoutube(youtubeId: String) {
-        if let youtubeURL = URL(string: "youtube://\(youtubeId)"),
+        // LINK NOT WORKING
+        let youtubeID = "UC_eGnmI2MqiwOLncMVLKbOg"
+        
+        // If YouTube is installed
+        if let youtubeURL = URL(string: "youtube://\(youtubeID)"),
             UIApplication.shared.canOpenURL(youtubeURL) {
-            // redirect to app
             UIApplication.shared.open(youtubeURL, options: [:], completionHandler: nil)
-        } else if let youtubeURL = URL(string: "https://www.youtube.com/watch?v=\(youtubeId)") {
-            // redirect through safari
+        }
+        // Otherwise, go to Safari
+        else if let youtubeURL = URL(string: "https://www.youtube.com/watch?v=\(youtubeID)") {
             UIApplication.shared.open(youtubeURL, options: [:], completionHandler: nil)
         }
     }
