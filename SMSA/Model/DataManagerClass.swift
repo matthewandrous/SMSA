e95c76160adf250b8e5363062a4ae82edfba4f1e
Pavly Habashy <phabashy@usc.edu>
2019-10-25 22:22:03 -0700

    fixed UI for announcements, sermons, and now playing

---
+++ SMSA/Model/DataManagerClass.swift
@@ -0,0 +1,16 @@
+//
+//  DataManagerClass.swift
+//  SMSA
+//
+//  Created by Pavly Habashy on 10/25/19.
+//  Copyright © 2019 SMSA Devs. All rights reserved.
+//
+
+import Foundation
+
+class DataManager {
+
+    static let shared = DataManager()
+    var SermonsTVC = SermonsTableViewController()
+    
+}
